2ff5b8a7da09bdd7310efae2a530b130a772658c
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-07-14 00:57:56 +0100

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print size of data
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %1u byte(s)\n", sizeof(a));
+	printf("Size of an int: %1u byte(s)\n", sizeof(b));
+	printf("Size of a long int: %1u byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %1u byte(s)\n", sizeof(d));
+	printf("Size of a float: %1u byte(s)\n", sizeof(e));
+	return (0);
+}
