43dbfb88f29ba4e44fcc0ad7ac367d7e08a0d25f
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-07-14 20:43:21 +0100

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - main function here
+ * main - main function
  *
- * Return: always (0)
+ * Return: Always 0
  */
 
 int main(void)
 {
-	char i;
+	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i);
-	putchar('\n');
+		printf("%d", i);
+	printf("\n");
 	return (0);
 }
