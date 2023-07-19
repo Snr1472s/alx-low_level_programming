b201f89a20e9d5a8b58a7b6cc63ea5ac53954d58
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-07-19 13:32:21 +0100

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number to start from
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i;
