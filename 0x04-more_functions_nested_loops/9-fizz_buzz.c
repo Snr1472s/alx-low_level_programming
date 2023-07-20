a2540ffbe882e034a10a7dfe84bcbfd28ae4a1d5
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-07-20 19:52:35 +0100

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - prints 1 to 100 and fizzbuss
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int w;
+
+	for (w = 1; w <= 100; w++)
+	{
+		if ((w % 3) == 0 && (w % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (w % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (w % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", w);
+		}
+
+
+	}
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
