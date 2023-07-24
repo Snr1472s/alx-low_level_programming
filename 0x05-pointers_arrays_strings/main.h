29b06b62f149b651ffc0db7dbe19343d0b7e008e
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-07-24 19:20:49 +0100

    8-print_array.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,5 +11,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
