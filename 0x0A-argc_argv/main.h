8afdf8c83e3e6dd45a27c67b5d9097842fe66dc9
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-04 20:17:46 +0100

    2-args.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,9 @@
 #define _MAIN_H_
 #include <stdio.h>
 
+int _putchar(char c);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
 
 #endif
