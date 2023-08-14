4dbfc4800e7a4c13f36041337602a44115cf11ea
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-14 14:09:45 +0100

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * file -  4-sum.h
+ * Desc: computes the sum of the numbers x and y
+ * @x: x parameter
+ * @y: y parameter
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
