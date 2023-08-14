8fb467a2984eebd7637ccfc1fc50a06ce5b64011
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-14 13:23:32 +0100

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * file -  0-object_like_macro.h
+ * Desc: header_file
+ * @size: bytes parameter
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/mian.h
@@ -0,0 +1,8 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif
