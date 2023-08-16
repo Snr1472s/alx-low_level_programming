7ddf5a34c1607b61dc64e5ddda8145c4d54723d7
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-16 18:12:07 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name -  a function that prints name
+ *
+ * @f: variable to hold the string name
+ * @name: name of person
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
