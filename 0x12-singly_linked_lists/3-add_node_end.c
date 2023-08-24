3ed5bfd58c7cfe2d45497c31c3e187f53411d102
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-25 00:42:51 +0100

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,6 +18,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
+	new_node->next = NULL;
 
 	if (!(*head))
 	{
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: parameter
+ *
+ * Return: (0)
+ */
+
+void free_list(list_t *head)
+{
+
