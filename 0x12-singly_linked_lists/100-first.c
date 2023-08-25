857874d730e17f266f8fc3c888783a043251c315
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-25 01:41:26 +0100

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n
+ *
+ * Return: (0)
+ */
+
+void before_main(void) __attribute__((constructor));
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
