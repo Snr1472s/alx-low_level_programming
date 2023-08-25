0aa643cd088ae4345d0c0aac6ef7e7631696c089
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-25 01:44:13 +0100

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,8 +8,7 @@
  * Return: (0)
  */
 
-void before_main(void) __attribute__((constructor));
-void before_main(void)
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
