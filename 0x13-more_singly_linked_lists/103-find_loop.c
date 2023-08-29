23ade0529c7a6a200b456405d7f44a204bab3a9c
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-08-29 22:24:34 +0100

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,40 @@
 #include "lists.h"
-
 /**
- * *find_listint_loop - finds loop in a linked list
- *
- * @head: parameter
- * Return: address of node where the loop starts, or NULL if there is no loop
+ * find_listint_loop - function that inds the loop in a linked list
+ * @head: head of a list
+ * Return: the address of the node where the loop starts
  */
-
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head, *fast = head;
+	listint_t *snr;
+	listint_t *obi;
 
-	if (!head)
-		return (NULL);
-
-	while (slow && fast && fast->next)
+	snr = head;
+	obi = head;
+	while (head && snr && snr->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		head = head->next;
+		snr = snr->next->next;
+
+		if (head == snr)
 		{
-			slow = head;
-			while (slow != fast)
+			head = obi;
+			obi = snr;
+			while (1)
 			{
-				slow = slow->next;
-				fast = fast->next;
-				if (slow == fast)
-					return (slow);
+				snr = obi;
+				while (snr->next != head && snr->next != obi)
+				{
+					snr = snr->next;
+				}
+				if (snr->next == head)
+					break;
+
+				head = head->next;
 			}
+			return (snr->next);
 		}
 	}
+
 	return (NULL);
 }
