db3b9b96071891bcf9edc5d0a24e5f033a98c59e
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-09-05 00:12:24 +0100

    all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - reads text file and prints it to POSIX standard /o
+ * read_textfile - function that reads a text file and prints it
+ * to the POSIX stdout.
  *
  * @filename: parameter
  * @letters: parameter
- * Return: (0)
+ * Return: no of letters printed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t x = 0;
-	int fd, w;
-	void *buff = malloc(sizeof(char) * 2);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
 
-	if (filename == NULL || buff == NULL)
+	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+
+	if (fd == -1)
 		return (0);
-	for (x = 0; x < (ssize_t)letters; x++)
-	{
-		w = read(fd, buff, 1);
-		if (w == 0)
-			break;
-		w = write(STDOUT_FILENO, buff, 1);
-		if (w != 1)
-			return (0);
-	}
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
 
 	close(fd);
 	free(buff);
 
-	return (x);
+	return (nwr);
 }
