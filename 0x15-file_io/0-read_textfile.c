e406e803dae26c0d4cd743ced87a4a50470ae7ef
Snr1472s <obinomengodswillsnr1472@gmail.com>
2023-09-04 21:23:50 +0100

    imkrg

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,25 +12,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t x = 0;
 	int fd, w;
-	void *buf = malloc(sizeof(char) * 2);
+	void *buff = malloc(sizeof(char) * 2);
 
-	if (filename == NULL || buf == NULL)
+	if (filename == NULL || buff == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 	for (x = 0; x < (ssize_t)letters; x++)
 	{
-		w = read(fd, buf, 1);
+		w = read(fd, buff, 1);
 		if (w == 0)
 			break;
-		w = write(STDOUT_FILENO, buf, 1);
+		w = write(STDOUT_FILENO, buff, 1);
 		if (w != 1)
 			return (0);
 	}
 
 	close(fd);
-	free(buf);
+	free(buff);
 
 	return (x);
 }
